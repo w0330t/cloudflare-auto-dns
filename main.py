7fa6ebb856dba175943053fa93ca84963dda1335
Blueness <w0330t@gmail.com>
2023-07-10 10:18:55 +0800

    修改逻辑，添加注释

--- main.py
+++ main.py
@@ -65,53 +65,72 @@ def check_connect(test_url:str, max_delay:float) -> bool:
         return False
 
 
+async def check_and_update_domains(domain, best_ip, adguard):
+    """
+    检查并更新域名重写规则
+
+    Args:
+        domain (str): 要检查和更新的域名
+        best_ip (str): 最佳 IP 地址
+        adguard (AdGuardHome): AdGuardHome 客户端实例
+    """
+    # 获取当前 AdGuardHome 的域名重写列表
+    adguard_domain_list = await adguard.request(uri='rewrite/list')
+    
+    # 检查域名是否在重写列表中，如果存在则进行更新，否则进行添加
+    result = check_domain(domain=domain, adguard_domain_list=adguard_domain_list)
+    put_data = {
+        "answer": best_ip,
+        "domain": domain
+    }
+    if result is not None:
+        update_data = {
+            "target": result,
+            "update": put_data,
+        }
+        await adguard.request(uri='rewrite/update', method='PUT', json_data=update_data)
+    else:
+        await adguard.request(uri='rewrite/add', method='POST', json_data=put_data)
+
 
 async def main():
-    """主程序
     """
-    # 导入config.toml
+    主函数，用于执行主要逻辑
+    """
     config = toml.load('config.toml')
     logger = init_logger()
     os.chdir('CloudflareSpeedTest/')
 
     while True:
-        logger.info(f"Loop start.")
+        logger.info("Loop start.")
+
+        # 运行 CloudflareST 程序
         subprocess.run(['./CloudflareST', '-url', config['test_download_url'], '-t', config['test_ping_count'], '-tlr', config['test_packet_loss']])
 
-        # 读取结果
-        best_ip = pd.read_csv('result.csv').loc[0, 'IP 地址']
-        os.remove('result.csv')
+        try: 
+            # 读取并删除 result.csv 文件
+            best_ip = pd.read_csv('result.csv').loc[0, 'IP 地址']
+            os.remove('result.csv')
 
-        async with AdGuardHome(host=config['ad_guard_home_url'], 
-                            port=config['ad_guard_home_port'], 
-                            username=config['ad_guard_home_username'], 
-                            password=config['ad_guard_home_passwd']) as adguard:
-            list = await adguard.request(uri='rewrite/list')
+            async with AdGuardHome(host=config['ad_guard_home_url'],
+                                port=config['ad_guard_home_port'],
+                                username=config['ad_guard_home_username'],
+                                password=config['ad_guard_home_passwd']) as adguard:
+                tasks = []
+                for domain in config['domains']:
+                    # 创建并发任务来检查和更新域名重写规则
+                    task = asyncio.create_task(check_and_update_domains(domain, best_ip, adguard))
+                    tasks.append(task)
+                await asyncio.gather(*tasks)
 
-            for domain in config['domains']:
-                result = check_domain(domain=domain, adguard_domain_list=list)
-                put_data = {
-                            "answer": best_ip,
-                            "domain": domain
-                }
-                if result is not None:
-                    update_data = {
-                        "target": result,
-                        "update": put_data,
-                    }
-                    await adguard.request(uri='rewrite/update', method='PUT', json_data=update_data)
-                    # logger.info(f'{domain} has been updated, ip is {best_ip}')
-                else:
-                    await adguard.request(uri='rewrite/add', method='POST', json_data=put_data)
-                    # logger.info(f'{domain} has been added, ip is {best_ip}')
+                logger.info(f"Update Cloudflare IP is {best_ip}")
 
-            list = await adguard.request(uri='rewrite/list')
-            # logger.debug(list)
-
-            logger.info(f"Update Cloudflare IP is {best_ip}")
-            
             if check_connect(config['test_url'], config['test_max_delay']):
                 await asyncio.sleep(60)
+
+        except FileExistsError as e:
+            logger.warning(str(e))
+            continue
 
 
 if __name__ == "__main__":
